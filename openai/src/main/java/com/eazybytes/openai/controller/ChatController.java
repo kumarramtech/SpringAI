43d70b64111cf205a0e7f14009ee6c1e5e47522e
Kumar <kumarramtech@gmail.com>
2025-12-02 22:07:19 +0530

    Added Project

---
+++ openai/src/main/java/com/eazybytes/openai/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.eazybytes.openai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+	
+	private final ChatClient chatClient;
+
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+	
+	@GetMapping("/chat")
+	public String chat(@RequestParam("message")String message) {
+		
+		return chatClient
+                .prompt()
+                //.advisors(new TokenUsageAuditAdvisor())
+                .user(message)
+                .call().content();
+	}
+}
